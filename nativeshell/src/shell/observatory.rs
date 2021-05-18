8b994834e1f189a9643ecade40f4057dfb9947f4
Matej Knopp <matej.knopp@gmail.com>
2021-05-18 20:27:05 +0200

    Fix observatory.rs build on windows

--- nativeshell/src/shell/observatory.rs
+++ nativeshell/src/shell/observatory.rs
@@ -4,12 +4,17 @@ fn have_observatory_url(url: &str) {
     eprintln!("Observatory URL '{}'", url);
 }
 
+// TODO(knopp): handle possible eintr in dup and dup2
 pub fn register_observatory_listener() {
     let stdout = unsafe { libc::dup(1) };
     let mut pipe = [0; 2];
     unsafe {
-        // TODO(knopp): handle possible eintr
+        #[cfg(target_family = "windows")]
+        libc::pipe(pipe.as_mut_ptr(), 1, libc::O_NOINHERIT);
+
+        #[cfg(target_family = "unix")]
         libc::pipe(pipe.as_mut_ptr());
+
         libc::close(1);
         libc::dup2(pipe[1], 1);
     }
@@ -20,10 +25,20 @@ pub fn register_observatory_listener() {
         const URL_PREFIX: &str = "flutter: Observatory listening on ";
         loop {
             let read = unsafe {
+                #[cfg(target_family = "windows")]
+                let read = libc::read(pipe[0], buf.as_mut_ptr() as *mut _, buf.len() as u32);
+
+                #[cfg(target_family = "unix")]
                 let read = libc::read(pipe[0], buf.as_mut_ptr() as *mut _, buf.len());
+
                 if read < 0 {
                     panic!("Could not read from stdout");
                 }
+
+                #[cfg(target_family = "windows")]
+                libc::write(stdout, buf.as_ptr() as *const _, read as u32);
+
+                #[cfg(target_family = "unix")]
                 libc::write(stdout, buf.as_ptr() as *const _, read as usize);
                 read
             };
