c36792c49919841f620946495c5fddd573f73d8e
Matej Knopp <matej.knopp@gmail.com>
2021-06-13 22:03:03 +0200

    Add NativeShell Plugin

---
+++ nativeshell/src/shell/plugin.rs
@@ -0,0 +1,85 @@
+use std::{cell::RefCell, rc::Rc};
+
+use crate::codec::{MethodCall, MethodCallReply, MethodInvoker, Value};
+
+use super::{Context, EngineHandle};
+
+#[derive(Clone)]
+pub struct MethodInvokerProvider {
+    context: Rc<Context>,
+    channel: String,
+}
+
+impl MethodInvokerProvider {
+    pub fn get_method_invoker_for_engine(
+        &self,
+        handle: EngineHandle,
+    ) -> Option<MethodInvoker<Value>> {
+        return self
+            .context
+            .message_manager
+            .borrow()
+            .get_method_invoker(handle, &self.channel);
+    }
+}
+
+pub trait MethodCallHandler {
+    fn on_method_call(
+        &mut self,
+        call: MethodCall<Value>,
+        reply: MethodCallReply<Value>,
+        engine: EngineHandle,
+    );
+
+    // keep the method invoker provider if you want to call methods on engines
+    fn set_method_invoker_provider(&mut self, _provider: MethodInvokerProvider) {}
+}
+
+// Convenience interface for implementing custom method call chandlers
+pub struct Plugin {
+    context: Rc<Context>,
+    channel: String,
+    _method_call_handler: Rc<RefCell<Box<dyn MethodCallHandler>>>,
+}
+
+impl Plugin {
+    pub fn new<H>(context: Rc<Context>, channel: &str, handler: H) -> Self
+    where
+        H: MethodCallHandler + 'static,
+    {
+        let res = Self {
+            context: context.clone(),
+            channel: channel.into(),
+            _method_call_handler: Rc::new(RefCell::new(Box::new(handler))),
+        };
+        res._method_call_handler
+            .as_ref()
+            .borrow_mut()
+            .set_method_invoker_provider(MethodInvokerProvider {
+                context: context.clone(),
+                channel: channel.into(),
+            });
+        let weak_handler = Rc::downgrade(&res._method_call_handler);
+        context
+            .message_manager
+            .borrow_mut()
+            .register_method_handler(channel, move |call, reply, engine| {
+                if let Some(handler) = weak_handler.upgrade() {
+                    handler
+                        .as_ref()
+                        .borrow_mut()
+                        .on_method_call(call, reply, engine);
+                }
+            });
+        res
+    }
+}
+
+impl Drop for Plugin {
+    fn drop(&mut self) {
+        self.context
+            .message_manager
+            .borrow_mut()
+            .unregister_method_handler(&self.channel);
+    }
+}
