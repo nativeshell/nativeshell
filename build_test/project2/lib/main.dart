581baf6f6a52b10b7c182c73409dc9257536b2a8
Matej Knopp <matej.knopp@gmail.com>
2021-07-08 10:41:43 +0200

    Build test WIP1

---
+++ build_test/project2/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:nativeshell/nativeshell.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTextStyle(
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+        ),
+        child: Container(
+          color: Colors.black,
+          child: WindowWidget(
+            onCreateState: (initData) {
+              WindowState? state;
+              state ??= MainWindowState();
+              return state;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainWindowState extends WindowState {
+  @override
+  WindowSizingMode get windowSizingMode =>
+      WindowSizingMode.atLeastIntrinsicSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Center(child: Text('Welcome to NativeShell!')),
+    );
+  }
+}
