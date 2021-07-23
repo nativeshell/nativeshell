2f9b14c3f5bc2e7809452b3406445f8496724996
Matej Knopp <matej.knopp@gmail.com>
2021-07-23 17:16:03 +0200

    Make clippy happy

--- nativeshell/src/shell/platform/win32/app_delegate.rs
+++ nativeshell/src/shell/platform/win32/app_delegate.rs
@@ -5,7 +5,7 @@ pub trait ApplicationDelegate {}
 pub struct ApplicationDelegateManager {}
 
 impl ApplicationDelegateManager {
-    pub fn new(context: &ContextRef) -> Self {
+    pub fn new(_context: &ContextRef) -> Self {
         Self {}
     }
     pub fn set_delegate<D: ApplicationDelegate + 'static>(&self, _delegate: D) {}
