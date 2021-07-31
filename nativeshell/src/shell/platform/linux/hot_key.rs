1b598e24243922c23da2f49f1a41d6d6424285b5
Matej Knopp <matej.knopp@gmail.com>
2021-07-31 23:02:02 +0200

    Linux implementation

--- nativeshell/src/shell/platform/linux/hot_key.rs
+++ nativeshell/src/shell/platform/linux/hot_key.rs
@@ -4,32 +4,32 @@ use crate::shell::{
     api_model::Accelerator, Context, EngineHandle, HotKeyHandle, HotKeyManagerDelegate,
 };
 
-use super::error::PlatformResult;
+use super::error::{PlatformError, PlatformResult};
 
 pub(crate) struct PlatformHotKeyManager {}
 
 impl PlatformHotKeyManager {
-    pub fn new(context: Context, delegate: Weak<RefCell<dyn HotKeyManagerDelegate>>) -> Self {
+    pub fn new(_context: Context, _delegate: Weak<RefCell<dyn HotKeyManagerDelegate>>) -> Self {
         Self {}
     }
 
-    pub fn assign_weak_self(&self, weak: Weak<PlatformHotKeyManager>) {}
+    pub fn assign_weak_self(&self, _weak: Weak<PlatformHotKeyManager>) {}
 
     pub fn create_hot_key(
         &self,
-        accelerator: Accelerator,
-        virtual_key: i64,
-        handle: HotKeyHandle,
-        engine: EngineHandle,
+        _accelerator: Accelerator,
+        _virtual_key: i64,
+        _handle: HotKeyHandle,
+        _engine: EngineHandle,
     ) -> PlatformResult<()> {
-        Ok(())
+        Err(PlatformError::NotAvailable)
     }
 
-    pub fn destroy_hot_key(&self, handle: HotKeyHandle) -> PlatformResult<()> {
-        Ok(())
+    pub fn destroy_hot_key(&self, _handle: HotKeyHandle) -> PlatformResult<()> {
+        Err(PlatformError::NotAvailable)
     }
 
-    pub fn engine_destroyed(&self, engine: EngineHandle) -> PlatformResult<()> {
+    pub fn engine_destroyed(&self, _engine: EngineHandle) -> PlatformResult<()> {
         Ok(())
     }
 }
