e2e9a170e52fc39b8fecd7409cc8c245dadeec35
Matej Knopp <matej.knopp@gmail.com>
2021-09-05 21:38:48 +0200

    Add nativeshell::spawn

--- nativeshell/src/shell/run_loop.rs
+++ nativeshell/src/shell/run_loop.rs
@@ -1,18 +1,33 @@
-use std::{rc::Rc, time::Duration};
+use std::{
+    cell::UnsafeCell,
+    future::Future,
+    rc::Rc,
+    sync::Arc,
+    thread::{self, ThreadId},
+    time::Duration,
+};
+
+use futures::{
+    future::LocalBoxFuture,
+    task::{waker_ref, ArcWake},
+    FutureExt,
+};
 
 use super::{
     platform::run_loop::{PlatformRunLoop, PlatformRunLoopSender},
-    Handle,
+    Context, ContextRef, Handle,
 };
 
 pub struct RunLoop {
     pub(super) platform_run_loop: Rc<PlatformRunLoop>,
+    context: Context,
 }
 
 impl RunLoop {
-    pub fn new() -> Self {
+    pub fn new(context: &ContextRef) -> Self {
         Self {
             platform_run_loop: Rc::new(PlatformRunLoop::new()),
+            context: context.weak(),
         }
     }
 
@@ -38,6 +53,8 @@ impl RunLoop {
     }
 
     pub fn run(&self) {
+        // set context as current
+        let _handle = self.context.get().unwrap().set_current();
         self.platform_run_loop.run()
     }
 
@@ -49,6 +66,19 @@ impl RunLoop {
         RunLoopSender {
             platform_sender: self.platform_run_loop.new_sender(),
         }
+    }
+
+    // Spawn the future with current run loop being the executor;
+    // Generally the future will be executed synchronously until first
+    // await.
+    pub fn spawn(&self, future: impl Future<Output = ()> + 'static) {
+        let future = future.boxed_local();
+        let task = Arc::new(Task {
+            sender: self.new_sender(),
+            thread_id: thread::current().id(),
+            future: UnsafeCell::new(future),
+        });
+        ArcWake::wake_by_ref(&task);
     }
 }
 
@@ -64,5 +94,44 @@ impl RunLoopSender {
         F: FnOnce() + 'static + Send,
     {
         self.platform_sender.send(callback)
+    }
+}
+
+//
+//
+//
+
+struct Task {
+    sender: RunLoopSender,
+    thread_id: ThreadId,
+    future: UnsafeCell<LocalBoxFuture<'static, ()>>,
+}
+
+//
+unsafe impl Send for Task {}
+unsafe impl Sync for Task {}
+
+impl Task {
+    fn poll(self: &std::sync::Arc<Self>) {
+        let waker = waker_ref(self).clone();
+        let context = &mut core::task::Context::from_waker(&waker);
+        unsafe {
+            let future = &mut *self.future.get();
+            let _ = future.as_mut().poll(context);
+        }
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &std::sync::Arc<Self>) {
+        let arc_self = arc_self.clone();
+        if arc_self.thread_id == thread::current().id() {
+            arc_self.poll();
+        } else {
+            let sender = arc_self.sender.clone();
+            sender.send(move || {
+                arc_self.poll();
+            });
+        }
     }
 }
