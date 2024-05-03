d0a9986ef82c58e366490d6a6274097dff4ef2ce
Matej Knopp <matej.knopp@gmail.com>
2024-05-04 00:18:31 +0200

    feat: test_project2 should create whole bundle

--- build_test/project2/build.rs
+++ build_test/project2/build.rs
@@ -1,9 +1,21 @@
-use nativeshell_build::{BuildResult, Flutter, FlutterOptions};
+use nativeshell_build::{AppBundleOptions, BuildResult, Flutter, FlutterOptions, MacOSBundle};
 
 fn build_flutter() -> BuildResult<()> {
     Flutter::build(FlutterOptions {
         ..Default::default()
     })?;
+
+    if cfg!(target_os = "macos") {
+        let options = AppBundleOptions {
+            bundle_name: "BuildTest.app".into(),
+            bundle_display_name: "Build Test".into(),
+            icon_file: "icons/AppIcon.icns".into(),
+            ..Default::default()
+        };
+        let resources = MacOSBundle::build(options)?;
+        resources.mkdir("icons")?;
+        resources.link("resources/mac_icon.icns", "icons/AppIcon.icns")?;
+    }
 
     Ok(())
 }
