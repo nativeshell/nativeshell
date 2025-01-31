2e6bfa09ff714ec8cebfaf0ed7228a62a39fd907
Matej Knopp <matej.knopp@gmail.com>
2025-01-31 10:47:36 +0100

    fix build

--- nativeshell_build/src/flutter_build.rs
+++ nativeshell_build/src/flutter_build.rs
@@ -13,7 +13,7 @@ use crate::{
     artifacts_emitter::ArtifactsEmitter,
     error::BuildError,
     plugins::Plugins,
-    util::{copy_to, find_executable, get_artifacts_dir, run_command},
+    util::{copy, copy_to, find_executable, get_artifacts_dir, run_command},
     BuildResult, FileOperation, IOResultExt,
 };
 
@@ -221,6 +221,13 @@ impl Flutter<'_> {
 
         Self::copy(&package_config, &package_config_out)?;
 
+        let root = package_config.parent().unwrap().parent().unwrap();
+        let lib = root.join("lib");
+        if lib.exists() {
+            println!("EXISTS");
+            copy(lib, flutter_out_root.join("lib"), true)?;
+        }
+
         let mut local_roots = HashSet::<PathBuf>::new();
 
         self.update_package_config_paths(package_config, package_config_out, &mut local_roots)?;
@@ -236,6 +243,7 @@ impl Flutter<'_> {
             &self.root_dir.join("pubspec.yaml"),
             &flutter_out_root.join("pubspec.yaml"),
         )?;
+        copy_to(self.root_dir.join("pubspec.lock"), &flutter_out_root, true)?;
 
         self.set_flutter_root()?;
         self.remove_rustc_from_env()?;
@@ -423,6 +431,9 @@ impl Flutter<'_> {
                 source: e,
             })?;
 
+        let original_parent = original.as_ref().parent().unwrap().parent().unwrap();
+        let new_parent = new.as_ref().parent().unwrap().parent().unwrap();
+
         for package in &mut package_config.packages {
             if package.root_uri.starts_with("..") {
                 // relative path
@@ -441,7 +452,12 @@ impl Flutter<'_> {
                 }
 
                 // remove unc from windows canonicalize
-                let absolute = simplified(&absolute);
+                let mut absolute = simplified(&absolute);
+
+                if absolute == original_parent {
+                    absolute = new_parent;
+                }
+
                 let mut absolute = absolute.to_slash_lossy();
                 if !absolute.starts_with('/') {
                     absolute = format!("/{absolute}");
